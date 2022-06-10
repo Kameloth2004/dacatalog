a7934aba25ff63dfd94e7c27967a89f846cc7a4f
Kameloth <kameloth2004@hotmail.com>
2022-06-10 15:10:16 -0300

    FormatPrice

--- frontweb/src/components/ProductPrice/index.tsx
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -1,3 +1,4 @@
+import { formatPrice } from 'util/formatters';
 import './styles.css';
 
 type Props ={
@@ -12,7 +13,7 @@ const ProductPrice = ( { price } : Props) => {
 
         <div className="product-price-container">
             <span>R$</span>
-            <h3>{ price }</h3>
+            <h3>{formatPrice(price)}</h3>
         </div>
     );
 
