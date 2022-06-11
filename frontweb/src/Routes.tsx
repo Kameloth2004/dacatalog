63d24ef9995a2e5d9dc1457086aaeb40c4911026
Kameloth <kameloth2004@hotmail.com>
2022-06-11 07:20:59 -0300

    ProductDetails route

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -3,7 +3,7 @@ import Home from 'pages/Home';
 import Navbar from 'components/Navbar';
 import Catalog from 'pages/Catalog';
 import Admin from 'pages/Admin';
-
+import ProductDetails from 'pages/ProductDetails';
 const Routes = () => (
   <BrowserRouter>
     <Navbar />
@@ -11,8 +11,11 @@ const Routes = () => (
       <Route path="/" exact>
         <Home />
       </Route>
-      <Route path="/products">
+      <Route path="/products" exact>
         <Catalog />
+      </Route>
+      <Route path="/products/:productId">
+        <ProductDetails />
       </Route>
       <Route path="/admin">
         <Admin />
---
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -0,0 +1,9 @@
+const ProductDetails = () => {
+
+    return (
+
+        <h1>Página de detalhes</h1>
+    );
+}
+
+export default ProductDetails;
