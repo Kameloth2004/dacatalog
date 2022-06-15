9b8cfda2db3369436ea6309f8290f0a27b94517d
Kameloth <kameloth2004@hotmail.com>
2022-06-15 14:43:55 -0300

    Pagination styling

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,6 +1,8 @@
+import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
+import './styles.css';
 
 const Catalog = () => {
   const product: Product = {
@@ -25,8 +27,9 @@ const Catalog = () => {
   };
 
   return (
-    <div className="container my-4">
-      <div className="row">
+    <div className="container my-4 catalog-container">
+      <div className="row catalog-title-container">
+        <h1>Catálogo de Produtos</h1>
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <Link to="/products/1">
             <ProductCard product={product} />
@@ -53,6 +56,11 @@ const Catalog = () => {
           </Link>
         </div>
       </div>
+
+      <div className='row'>
+        <Pagination />
+      </div>
+
     </div>
   );
 };
