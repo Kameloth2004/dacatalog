824fe26b70373cc30ad34687c057d771db717dec
Kameloth <kameloth2004@hotmail.com>
2022-06-21 13:15:24 -0300

    Catalog listing

---
+++ frontweb/src/types/vendor/spring.ts
@@ -0,0 +1,13 @@
+
+
+export type SpringPage<T> = {
+    content: T[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size: number;
+    number: number;
+    first: boolean;
+    numerOfElements?: number;
+    empty: boolean;
+};
