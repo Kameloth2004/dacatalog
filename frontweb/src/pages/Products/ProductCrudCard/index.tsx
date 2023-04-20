594cfd5a7c39a30e9397c256174454d3a8e46814
Adriano Marques da Silva <kameloth2004@hotmail.com>
2023-04-20 14:56:07 -0300

    CRUD routes

--- frontweb/src/pages/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Products/ProductCrudCard/index.tsx
@@ -2,6 +2,7 @@ import './styles.css';
 import ProductPrice from 'components/ProductPrice';
 import { Product } from 'types/product';
 import CategoryBadge from '../CategoryBadge';
+import { Link } from 'react-router-dom';
 
 type Props = {
   product: Product;
@@ -28,9 +29,14 @@ const ProductCrudCard = ({ product }: Props) => {
         <button className='btn btn-outline-danger product-crud-card-button product-crud-card-button-first'>
             EXCLUIR
         </button>
+        
+        <Link to={`/admin/products/${product.id}`}>
         <button className='btn btn-outline-secundary product-crud-card-button'>
             EDITAR
-        </button>    
+        </button>         
+        </Link>
+        
+           
       </div>
     </div>
   );
