46b0af2d644fddc1960b46accca33c6ed799f3dc
Adriano Marques da Silva <kameloth2004@hotmail.com>
2023-04-21 15:26:38 -0300

    Float Format

--- frontweb/src/pages/Products/Form/index.tsx
+++ frontweb/src/pages/Products/Form/index.tsx
@@ -7,6 +7,7 @@ import Select from 'react-select';
 import './styles.css';
 import { useEffect, useState } from 'react';
 import { Category } from 'types/category';
+import CurrencyInput from 'react-currency-input-field';
 
 type UrlParams = {
   productId: string;
@@ -50,11 +51,15 @@ const Form = () => {
   }, [isEditing, productId, setValue]);
 
   const onSubmit = (formData: Product) => {
-    
+    const data = {
+      ...formData,
+      price: String(formData.price).replace(',', '.'),
+    };
+
     const config: AxiosRequestConfig = {
       method: isEditing ? 'PUT' : 'POST',
       url: isEditing ? `/products/${productId}` : '/products',
-      data: formData,
+      data,
       withCredentials: true,
     };
 
@@ -119,30 +124,36 @@ const Form = () => {
               )}
 
               <div className="margin-bottom-30">
-                <input
-                  {...register('price', {
-                    required: 'Campo obrigatorio',
-                  })}
-                  type="text"
-                  className={`form-control base-input ${
-                    errors.name ? 'is-invalid' : ''
-                  } `}
-                  placeholder="Preço"
+                <Controller
                   name="price"
+                  rules={{ required: 'Campo obrigatorio' }}
+                  control={control}
+                  render={({ field }) => (
+                    <CurrencyInput
+                      placeholder="Preço"
+                      className={`form-control base-input ${
+                        errors.name ? 'is-invalid' : ''
+                      }`}
+                      disableGroupSeparators={true}
+                      value={field.value}
+                      onValueChange={field.onChange}
+                    />
+                  )}
                 />
 
                 <div className="invalid-feedback d-block">
                   {errors.price?.message}
                 </div>
               </div>
+
               <div className="margin-bottom-30">
                 <input
                   {...register('imgUrl', {
                     required: 'Campo obrigatorio',
-                    pattern:{
+                    pattern: {
                       value: /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm,
-                      message: 'Deve ser uma url válida'
-                    }
+                      message: 'Deve ser uma url válida',
+                    },
                   })}
                   type="text"
                   className={`form-control base-input ${
